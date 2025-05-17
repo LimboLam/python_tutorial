058813ab25f3fa614a6ab4cbb4b6bdbd211a5f9f
LimboLam <liam.xiong24@student.cart.org>
2025-05-16 21:11:32 -0700

    stuff

---
+++ variables.py
@@ -0,0 +1,20 @@
+# Variable = A container for a value (string, interger, float, boolean)
+
+# Strings
+first_name = 'Liam'
+food = 'apple'
+email = "email69@gmail.com"
+
+print(f'Hello {first_name}')
+print(f'You like {food}')
+print(f'Your email is {email}')
+
+
+# Intergers
+
+age = 17
+quantity = 3
+num_of_students
+
+print(f'You are {age} years old')
+print(f'You are buying {quantity} items')
