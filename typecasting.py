f49ccb6067c634f8af2c7a1632d007d1f6d19f4e
LimboLam <liam.xiong24@student.cart.org>
2025-05-17 17:33:18 -0700

    stufdf

---
+++ typecasting.py
@@ -0,0 +1,23 @@
+# typecasting = Turing a value to one data type to another
+#               Explicit vs Implicit
+
+name = 'Liam'
+age = 17
+gpa = 3.5
+is_student = True
+
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+# print(type(is_student))
+
+# Explicit Conversion
+
+age = float(age)
+print(type(age))
+
+gpa = int(gpa)
+print(type(gpa))
+
+is_student = str(is_student)
+print(type(is_student))
