c6e25a7885de7caf114c8aa71741e1b4a4c6fa10
LimboLam <liam.xiong24@student.cart.org>
2025-05-17 21:06:08 -0700

    stuff

---
+++ mathpy.py
@@ -0,0 +1,33 @@
+import math
+# Arithmetic Operators
+# friends = 4
+
+# # friends += 1
+# # friends -= 2
+# # friends *= 3
+# # friends /= 2
+# # friends **= 2
+# remainder = friends % 3
+
+# print(friends)
+# print(remainder)
+
+# Math Functions
+x = 3.14
+x = math.pi
+y = 4
+z = -5
+
+# result = round(x)
+# result = abs(z)
+# result = pow(y, 3)
+# result = max(x, y, z)
+# result = min(x, y, z)
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(y)
+# result = math.ceil(x)
+result = math.floor(x)
+
+print(result)
